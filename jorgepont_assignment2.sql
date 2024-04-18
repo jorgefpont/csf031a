179ae2d5ad3a94c569bfaf69305901f400b5c2ac
jorgefpont <jorgefpont@gmail.com>
2024-04-18 07:43:13 -0700

    started assignment2

---
+++ jorgepont_assignment2.sql
@@ -0,0 +1,64 @@
+/* Assignment2.sql 
+   Jorge Pont
+   CS 31A, Winter 2024
+*/
+
+/* run database building scripts */
+
+/* 
+In the terminal, need to go to this directory:
+c:\Program Files\MySQL\MySQL Server 8.0\bin
+and run:
+> .\mysql -u jorge -p
+Then source this script:
+SOURCE C:/Users/jorge/classes/CSF031A/jorgepont_assignment2.sql
+*/
+
+-- script to create and use the database
+SOURCE C:/Users/jorge/classes/CSF031A/megastore_database_scripts/create_tables_mega_store.sql
+
+-- script to populate the tables
+SOURCE C:/Users/jorge/classes/CSF031A/megastore_database_scripts/insert_data_mega_store.sql
+-- Let's see what we have
+SHOW DATABASES;
+SHOW TABLES;
+
+/* put the database name into this command */
+
+
+/* Query 1 */
+
+
+/* Query 2 */
+
+
+/* Query 3 */
+
+
+/* Query 4 */
+
+
+/* Query 5 */
+
+
+/* Query 6 */
+
+
+/* Query 7 */
+
+
+/* Query 8 */
+
+
+/* Query 9 */
+
+
+/*  Query 10 */
+
+
+/* Query 11 */
+
+
+/* Query 12 */
+
+
