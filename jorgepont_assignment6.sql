717909fcc39803aab7ee7ba674e997133664848c
jorgefpont <jorgefpont@gmail.com>
2024-05-20 18:48:55 -0700

    hwk 6 finished

--- jorgepont_assignment6.sql
+++ jorgepont_assignment6.sql
@@ -11,8 +11,14 @@ run this at the mysql prompt:
 SOURCE ~/jorge-files/Foothill-courses/csf031a/jorgepont_assignment5.sql
 */
 
+/* Linux
 SOURCE ~/jorge-files/Foothill-courses/csf031a/megastore_database_scripts/create_tables_mega_store.sql;
 SOURCE ~/jorge-files/Foothill-courses/csf031a/megastore_database_scripts/insert_data_mega_store.sql;
+*/
+
+/* Windows*/
+SOURCE C:/Users/jorge/classes/CSF031A/megastore_database_scripts/create_tables_mega_store.sql
+SOURCE C:/Users/jorge/classes/CSF031A/megastore_database_scripts/insert_data_mega_store.sql
 
 USE megastore;
 
@@ -101,7 +107,12 @@ Display the product ID and the number of orders placed for each product.
 Show the results in decreasing order and label result column NumOrders.
 */
 
-
+SELECT  p.prod_id, COUNT(od.ord_id) AS 'NumOrders'
+FROM products p
+INNER JOIN order_details od
+ON od.prod_id = p.prod_id
+GROUP BY p.prod_id
+ORDER BY COUNT(od.ord_id) DESC;
 
 /* Question 10
 Write a SELECT statement that answers this question: Which customers have ordered more
@@ -111,7 +122,13 @@ The customer ID from the CUSTOMERS table
 The count of distinct products from the customer’s orders
 */
 
-
+SELECT c.cust_id, COUNT(od.prod_id)
+FROM customers c
+INNER JOIN orders o
+ON c.cust_id = o.cust_id
+INNER JOIN order_details od
+ON o.ord_id = od.ord_id
+GROUP BY c.cust_id;
 
 /* Question 11
 Consider the groups of products where each group contains the products 
@@ -120,23 +137,67 @@ display category ID, number of products in the group, and average price
 of the products in the group. 
 */
 
-
+SELECT 
+	p.catg_id, 
+	COUNT(p.prod_id) AS 'number of products',
+	AVG(p.prod_list_price) AS 'average price of products'
+FROM products p
+GROUP BY p.catg_id
+HAVING COUNT(p.prod_id) > 5;
 
 /* Question 12
 Display the average price and category ID for each product category. 
 */
 
-
+SELECT 
+	AVG(prod_list_price) AS 'average product price', 
+	catg_id
+FROM products
+GROUP BY catg_id;
 
 /* Question 13
 Use UNION ALL to return orders with either a Sporting goods product or 
 a pet supply product or both.
 */
 
+(SELECT o.ord_id, od.prod_id, p.catg_id, p.prod_name
+FROM orders o
+JOIN order_details od
+ON o.ord_id = od.ord_id
+JOIN products p
+WHERE p.catg_id LIKE 'SPG')
 
+UNION ALL
+
+(SELECT o.ord_id, od.prod_id, p.catg_id, p.prod_name
+FROM orders o
+JOIN order_details od
+ON o.ord_id = od.ord_id
+JOIN products p
+WHERE p.catg_id LIKE 'PET')
+
+LIMIT 10;
 
 /* Question 14
 USE UNION to return HW products purchased with either a Nov or Dec date.
 */
 
+ prod_id | catg_id | prod_name       |
 
+SELECT od.prod_id, p.catg_id, p.prod_name, o.ord_date
+FROM orders o
+JOIN order_details od
+ON o.ord_id = od.ord_id
+JOIN products p
+WHERE p.catg_id LIKE 'HW' AND MONTH(o.ord_date) LIKE '11'
+
+UNION
+
+SELECT od.prod_id, p.catg_id, p.prod_name, o.ord_date
+FROM orders o
+JOIN order_details od
+ON o.ord_id = od.ord_id
+JOIN products p
+WHERE p.catg_id LIKE 'HW' AND MONTH(o.ord_date) LIKE '12'
+
+LIMIT 100;
